5eebd118ac082af579b41d435754d0afd0c79765
8682akash <99233617+8682akash@users.noreply.github.com>
2022-09-27 09:41:22 +0530

    commit

--- newprpgramm/sub.cs
+++ newprpgramm/sub.cs
@@ -8,14 +8,14 @@ namespace newprpgramm
 {
     internal class sub
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("enter the first number");
-                int x=Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("enter the second number");
-                int y = Convert.ToInt32(Console.ReadLine());
-                int z = x - y;
-               Console.WriteLine("substraction of two num is "+z);
-        }
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("enter the first number");
+        //        int x=Convert.ToInt32(Console.ReadLine());
+        //        Console.WriteLine("enter the second number");
+        //        int y = Convert.ToInt32(Console.ReadLine());
+        //        int z = x - y;
+        //       Console.WriteLine("substraction of two num is "+z);
+        //}
     }
 }
