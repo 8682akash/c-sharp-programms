b470758baaffd9e6ef27ef0080347133829de93e
8682akash <99233617+8682akash@users.noreply.github.com>
2022-09-27 09:35:21 +0530

    commit

--- newprpgramm/add.cs
+++ newprpgramm/add.cs
@@ -8,14 +8,14 @@ namespace newprpgramm
 {
     internal class add
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("enter the first number");
-            int x=Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enter the second number");
-            int y = Convert.ToInt32(Console.ReadLine());
-            int z = x + y;
-            Console.WriteLine("addition of two num is "+z);
-        }
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("enter the first number");
+        //    int x=Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("enter the second number");
+        //    int y = Convert.ToInt32(Console.ReadLine());
+        //    int z = x + y;
+        //    Console.WriteLine("addition of two num is "+z);
+        //}
     }
 }
---
+++ newprpgramm/sub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newprpgramm
+{
+    internal class sub
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the first number");
+                int x=Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter the second number");
+                int y = Convert.ToInt32(Console.ReadLine());
+                int z = x - y;
+               Console.WriteLine("substraction of two num is "+z);
+        }
+    }
+}
