34a24103f082184dca8ecf61089b17ff064b8425
8682akash <99233617+8682akash@users.noreply.github.com>
2022-09-27 23:10:55 +0530

    commit

--- newprpgramm/print1to100.cs
+++ newprpgramm/print1to100.cs
@@ -8,6 +8,9 @@ namespace newprpgramm
 {
     internal class print1to100
     {
+
+        //WAP to print 1 to 100 numbers
+
         public static void Main(string[] args)
         {
             for(int i = 1; i <= 100; i++)
