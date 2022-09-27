985da3534d229bd925eeb7c71d1844506abb108f
8682akash <99233617+8682akash@users.noreply.github.com>
2022-09-27 23:14:29 +0530

    commit

--- newprpgramm/print1to100.cs
+++ newprpgramm/print1to100.cs
@@ -11,12 +11,12 @@ namespace newprpgramm
 
         //WAP to print 1 to 100 numbers
 
-        public static void Main(string[] args)
-        {
-            for(int i = 1; i <= 100; i++)
-            {
-                Console.WriteLine(i);
-            }
-        }
+        //public static void Main(string[] args)
+        //{
+        //    for(int i = 1; i <= 100; i++)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //}
     }
 }
