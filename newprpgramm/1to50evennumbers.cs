35d5ba4c2a56b03157852fca012fbe9918dc9636
8682akash <99233617+8682akash@users.noreply.github.com>
2022-09-27 23:18:49 +0530

    commit

--- newprpgramm/1to50evennumbers.cs
+++ newprpgramm/1to50evennumbers.cs
@@ -8,15 +8,18 @@ namespace newprpgramm
 {
     internal class _1to50evennumbers
     {
-        public static void Main(string[] args)
-        {
-            for(int i = 1; i <= 50; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine(i);
-                }
-            }
-        }
+
+        //WAP to print 1 to 50 even numbers
+
+        //public static void Main(string[] args)
+        //{
+        //    for(int i = 1; i <= 50; i++)
+        //    {
+        //        if (i % 2 == 0)
+        //        {
+        //            Console.WriteLine(i);
+        //        }
+        //    }
+        //}
     }
 }
---
+++ newprpgramm/50to80evennumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newprpgramm
+{
+    internal class _50to80evennumbers
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 50; i <= 80; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
