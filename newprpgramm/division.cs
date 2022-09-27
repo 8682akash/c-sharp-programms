1de3083c4792c9174a19f1787e396d5c13b348fc
8682akash <99233617+8682akash@users.noreply.github.com>
2022-09-27 09:43:13 +0530

    commit

---
+++ newprpgramm/division.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newprpgramm
+{
+    internal class division
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("enter the first number");
+              int x=Convert.ToInt32(Console.ReadLine());
+               Console.WriteLine("enter the second number");
+                    int y = Convert.ToInt32(Console.ReadLine());
+                    int z = x / y;
+                 Console.WriteLine("division of two num is "+z);
+        }
+    }
+}
--- newprpgramm/multiplication.cs
+++ newprpgramm/multiplication.cs
@@ -8,14 +8,14 @@ namespace newprpgramm
 {
     internal class multiplication
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("enter the first number");
-                int x=Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("enter the second number");
-                int y = Convert.ToInt32(Console.ReadLine());
-                int z = x * y;
-             Console.WriteLine("multiplication of two num is "+z);
-        }
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("enter the first number");
+        //        int x=Convert.ToInt32(Console.ReadLine());
+        //        Console.WriteLine("enter the second number");
+        //        int y = Convert.ToInt32(Console.ReadLine());
+        //        int z = x * y;
+        //     Console.WriteLine("multiplication of two num is "+z);
+        //}
     }
 }
