472a2d22fdd192076caadaeb9ad5dc07f2ec0644
8682akash <99233617+8682akash@users.noreply.github.com>
2022-09-27 18:40:02 +0530

    commit

--- newprpgramm/grossalary.cs
+++ newprpgramm/grossalary.cs
@@ -17,21 +17,21 @@ namespace newprpgramm
             int BS=Convert.ToInt32(Console.ReadLine());
             if (BS <= 10000)
             {
-                int DA = 10000 / 100 * 80;
-                int HRA = 10000 / 100 * 20;
+                int DA = (BS / 100) * 80;
+                int HRA =(BS / 100) * 20;
                 int GS = BS + DA + HRA;
                 Console.WriteLine("gross salary is"+GS);
-            }else if(BS <= 15500)
+            }else if(BS>10000 || BS <= 20000)
             {
-                int DA1 = 15500 / 100 * 90;
-                int HRA1 = 15500 / 100 * 35;
+                int DA1 = (BS / 100) * 90;
+                int HRA1 = (BS / 100) * 35;
                 int GS1 = BS + DA1 + HRA1;
                 Console.WriteLine("gross salary is" + GS1);
             }
             else
             {
-                int DA2 = BS / 100 * 95;
-                int HRA2 = BS / 100 * 40;
+                int DA2 = (BS / 100) * 95;
+                int HRA2 = (BS / 100) * 40;
                 int GS2 = BS + DA2 + HRA2;
                 Console.WriteLine("gross salary is" + GS2);
             }
