118dece9f49ffea23cef2f42f2d2c9a41b40e095
8682akash <99233617+8682akash@users.noreply.github.com>
2022-09-28 09:00:27 +0530

    commit

--- newprpgramm/armstrong.cs
+++ newprpgramm/armstrong.cs
@@ -9,27 +9,29 @@ namespace newprpgramm
     internal class armstrong
     {
 
-        static void Main(string[] args)
-        {
-            Console.WriteLine("enter the any num to validate armstrong or not");
-            int num = Convert.ToInt32(Console.ReadLine()); //153
-            int temp = num;
-            int sum = 0, r;
-            while (num > 0)
-            {
-                r = num % 10; // 1
-                sum = sum + (r * r * r); //27 +125 +1
-                num = num / 10;   // 153/10  -> 15   15/10 -> 1
-            }
-            if (temp == sum)
-            {
-                Console.WriteLine("Armstrong");
-            }
-            else
-            {
-                Console.WriteLine("Not Armstrong");
-            }
-        }
+        //WAP to print number is armstrong or not
+
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("enter the any num to validate armstrong or not");
+        //    int num = Convert.ToInt32(Console.ReadLine()); //153
+        //    int temp = num;
+        //    int sum = 0, r;
+        //    while (num > 0)
+        //    {
+        //        r = num % 10; // 1
+        //        sum = sum + (r * r * r); //27 +125 +1
+        //        num = num / 10;   // 153/10  -> 15   15/10 -> 1
+        //    }
+        //    if (temp == sum)
+        //    {
+        //        Console.WriteLine("Armstrong");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Not Armstrong");
+        //    }
+        //}
 
     }
 }
---
+++ newprpgramm/primeornotprime.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace newprpgramm
+{
+    internal class primeornotprime
+    {
+        //WAP to check number is prime or not
+
+        public static void Main(string[] args)
+        {
+            int count = 0;
+            Console.WriteLine("enter the number");
+            int num=Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i <= num; i++)
+            {
+                if (num % i == 0) 
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                Console.WriteLine("prime number");
+            }
+            else
+            {
+                Console.WriteLine("not prime number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
