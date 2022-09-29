45b2a80043e7d21b24f043b68837b2e2859cc974
8682akash <99233617+8682akash@users.noreply.github.com>
2022-09-29 09:48:06 +0530

    commit

---
+++ newprpgramm/duck.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newprpgramm
+{
+    internal class duck
+    {
+        static void Main(string[] args)
+        {
+            int flag = 0;
+            int count = 0;
+            string str = Console.ReadLine();
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (str[0] == '0')
+                {
+                    flag = 1;
+                }
+                if (str[i] == '0')
+                {
+                    count++;
+                    flag = 0;
+                    break;
+                }
+
+            }
+            if (flag == 1)
+            {
+                Console.WriteLine("Not Duck");
+            }
+            else if (count > 0 || flag == 0)
+            {
+                Console.WriteLine("Duck");
+            }
+
+        }
+    }
+}
--- newprpgramm/pattern3.cs
+++ newprpgramm/pattern3.cs
@@ -8,17 +8,17 @@ namespace newprpgramm
 {
     internal class pattern3
     {
-        public static void Main(String[] args)
-        {
-            for (int i = 1; i <= 5; i--)
-            {
-                for (int j = 5; j <= i; j++)
-                {
-                    Console.Write(j);
-                }
-                Console.WriteLine();
-            }
+        //public static void Main(String[] args)
+        //{
+        //    for (int i = 1; i <= 5; i--)
+        //    {
+        //        for (int j = 5; j <= i; j++)
+        //        {
+        //            Console.Write(j);
+        //        }
+        //        Console.WriteLine();
+        //    }
 
-        }
+        //}
     }
 }
