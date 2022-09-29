7387ac2f0077d196a248ba6b05b201eb67e7119a
8682akash <99233617+8682akash@users.noreply.github.com>
2022-09-29 10:01:45 +0530

    commit

--- newprpgramm/duck.cs
+++ newprpgramm/duck.cs
@@ -8,34 +8,34 @@ namespace newprpgramm
 {
     internal class duck
     {
-        static void Main(string[] args)
-        {
-            int flag = 0;
-            int count = 0;
-            string str = Console.ReadLine();
-            for (int i = 1; i < str.Length; i++)
-            {
-                if (str[0] == '0')
-                {
-                    flag = 1;
-                }
-                if (str[i] == '0')
-                {
-                    count++;
-                    flag = 0;
-                    break;
-                }
+        //static void Main(string[] args)
+        //{
+        //    int flag = 0;
+        //    int count = 0;
+        //    string str = Console.ReadLine();
+        //    for (int i = 1; i < str.Length; i++)
+        //    {
+        //        if (str[0] == '0')
+        //        {
+        //            flag = 1;
+        //        }
+        //        if (str[i] == '0')
+        //        {
+        //            count++;
+        //            flag = 0;
+        //            break;
+        //        }
 
-            }
-            if (flag == 1)
-            {
-                Console.WriteLine("Not Duck");
-            }
-            else if (count > 0 || flag == 0)
-            {
-                Console.WriteLine("Duck");
-            }
+        //    }
+        //    if (flag == 1)
+        //    {
+        //        Console.WriteLine("Not Duck");
+        //    }
+        //    else if (count > 0 || flag == 0)
+        //    {
+        //        Console.WriteLine("Duck");
+        //    }
 
-        }
+        //}
     }
 }
---
+++ newprpgramm/ref.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newprpgramm
+{
+    //internal class @ref
+
+        //{
+        //    static void Swap(ref int a, ref int b)
+        //    {
+        //        int temp;
+        //        temp = a;
+        //        a = b;
+        //        b = temp;
+        //    }
+        //    static void Main(string[] args)
+        //    {
+
+        //        // call by ref
+        //        int a = 10, b = 20;
+        //        Swap(ref a, ref b);
+        //        Console.WriteLine($"a= {a}  b= {b}");
+        //    }
+        //}
+
+
+    }
+
