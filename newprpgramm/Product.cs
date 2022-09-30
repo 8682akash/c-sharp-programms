6fd1c6dd592b6e6236067565fe578e3bc35145e6
8682akash <99233617+8682akash@users.noreply.github.com>
2022-09-30 23:29:17 +0530

    commit

--- newprpgramm/Product.cs
+++ newprpgramm/Product.cs
@@ -7,33 +7,33 @@ using System.Threading.Tasks;
 
 namespace newprpgramm
 {
-    //internal class Product
-    //{
-    //    private int id,price;
-    //    private string name;
-    //    private double discount,FN,FN1;
+    internal class Product
+    {
+        private int id, price;
+        private string name;
+        private double discount, FN, FN1;
 
-    //    public void Dataadd(int id,string name,int price,double discount)
-    //    {
-    //        this.id = id;
-    //        this.name = name;
-    //        this.price = price;
-    //        this .discount = discount;
+        public void Dataadd(int id, string name, int price, double discount)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.discount = discount;
 
-    //    }
-    //    public void Finalprice()
-    //    {
-    //        FN = (double)price / 100 * discount;
-    //        FN1 = price - FN;
-          
-          
-    //    }
-    //    public string Display()
-    //    {
-    //        return $"id={id} name={name} price={price} discout={discount} finalprice={FN1}";
+        }
+        public void Finalprice()
+        {
+            FN = (double)price / 100 * discount;
+            FN1 = price - FN;
 
-    //    }
 
-    //}
-    
+        }
+        public string Display()
+        {
+            return $"id={id} name={name} price={price} discout={discount} finalprice={FN1}";
+
+        }
+
+    }
+
 }
