8b32638a89ca79010682fa90f1eda40d27928c73
8682akash <99233617+8682akash@users.noreply.github.com>
2022-10-18 10:39:34 +0530

    commit

--- newprpgramm/AlloperationOut.cs
+++ newprpgramm/AlloperationOut.cs
@@ -7,22 +7,22 @@ using System.Threading.Tasks;
 
 namespace newprpgramm
 {
-    class AlloperationOut
-    {
-        static void Calculation(int a,int b,out int sum,out int sub,out int mul)
-        {
-            sum= a + b;
-            sub= a - b;
-            mul= a * b;
+    //class AlloperationOut
+    //{
+    //    static void Calculation(int a,int b,out int sum,out int sub,out int mul)
+    //    {
+    //        sum= a + b;
+    //        sub= a - b;
+    //        mul= a * b;
 
-        }
-        static void Main(string[] args)
-        {
-            int a = 45, b = 23, sum, sub, mul;
-            Calculation(a,b,out sum,out sub,out mul);
-            Console.WriteLine(sum);
-            Console.WriteLine(sub);
-            Console.WriteLine(mul);
-        }
-    }
+    //    }
+    //    static void Main(string[] args)
+    //    {
+    //        int a = 45, b = 23, sum, sub, mul;
+    //        Calculation(a,b,out sum,out sub,out mul);
+    //        Console.WriteLine(sum);
+    //        Console.WriteLine(sub);
+    //        Console.WriteLine(mul);
+    //    }
+    //}
 }
